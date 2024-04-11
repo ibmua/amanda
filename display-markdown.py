e50eb07e78fae7d444b7a0407f885b865e82b9f4
Menshykov <ihor.ibm@gmail.com>
2024-04-11 21:35:47 +0300

    Add files via upload

--- display-markdown.py
+++ display-markdown.py
@@ -3,32 +3,53 @@ import re
 from rich.console import Console
 from rich.markdown import Markdown
 
+
 try:
-    import IPython
-    if 'google.colab' in str(IPython.get_ipython()):
-        is_colab = True
-        is_jupyter = False
-    else:
-        is_colab = False
-        is_jupyter = IPython.get_ipython() is not None
-except (ImportError, NameError):
-    is_colab = False
-    is_jupyter = False
+    import google.colab
+    in_colab = True
+except ImportError:
+    in_colab = False
+
+if in_colab:
+    print("Running in Google Colab")
+
+
+# print("in_colab",in_colab)
+
+# try:
+#     import IPython
+#     if 'google.colab' in str(IPython.get_ipython()):
+#         is_colab = True
+#         is_jupyter = False
+#     else:
+#         is_colab = False
+#         is_jupyter = IPython.get_ipython() is not None
+# except (ImportError, NameError):
+#     is_colab = False
+#     is_jupyter = False
+
+# print("is_colab",is_colab)
+# print("is_jupyter",is_jupyter)
+# is_colab = False
+# is_jupyter = True
 
 def display_markdown(file_path):
     console = Console()
     with open(file_path, 'r') as f:
         content = f.read()
-    if is_colab or is_jupyter:
-        # Replace links with plain text URLs in Colab or Jupyter
-        content = re.sub(r'\[([^\]]+)\]\(([^)]+)\)', r'\1 (\2)', content)
+    if in_colab:
+        console.print(content)
+        return
+    #     # Rewrite links in the desired format for Colab or Jupyter
+    #     content = re.sub(r'\[([^\]]+)\]\(([^)]+)\)', r'- "\1" - \2', content)
     md = Markdown(content)
-    if is_colab or is_jupyter:
-        # Print the Markdown content as plain text in Colab or Jupyter
-        print(md.plain)
-    else:
+    # if is_colab or is_jupyter:
+    #     # Print the Markdown content as plain text in Colab or Jupyter
+    #     print(md.plain)
+    # else:
         # Print the formatted Markdown in the terminal
-        console.print(md)
+    # console.print(md)
+    print(md)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Display markdown file in the terminal.')
